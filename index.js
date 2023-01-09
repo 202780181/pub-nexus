5812af6378198bd72430b1c2b8c72da21d722c56
longyadong <202780181@qq.com>
2023-01-09 19:08:25 +0800

    fix: update

--- index.js
+++ index.js
@@ -4,50 +4,74 @@ import fs from 'node:fs'
 import path from 'node:path'
 import spawn from 'cross-spawn'
 import prompts from 'prompts'
+import {
+  blue,
+  cyan,
+  green,
+  lightGreen,
+  lightRed,
+  magenta,
+  red,
+  reset,
+  yellow,
+} from 'kolorist'
 
 const cwd = process.cwd()
 
-const runSingleExec = async (name) => {
-  var mes = null
-  var err = null
-  if (!name || name === '') return;
+const defaultTargetDir = 'publish'
 
-  function setValue(key, value) {
-    console.log('key:')
-    console.log(key)
-    console.log('value:')
-    mes = value
-  }
-
-  function mkdir(dirPath, dirName) {
-    if(dirName !== path.dirname(dirPath)) {
-      mkdir(dirPath)
-      return;
-    }
-    if(fs.existsSync(dirName)) {
-      fs.mkdirSync(dirPath)
-    }else {
-      mkdir(dirName, path.dirname(dirName))
-      fs.mkdirSync(dirPath)
-    }
-  }
-
-  // const response = await prompts({
-  //   type: 'number',
-  //   name: 'value',
-  //   message: 'How old are you?',
-  //   validate: value => value < 18 ? `Nightclub is 18+ only` : true
-  // });
-  // console.log(response);
-
-  const { status } = spawn.sync('npm install', [], {
-    stdio: 'inherit',
-  })
-  process.exit(status ?? 0)
-
-  return {mes, err};
+function fileExist(path) {
+  return fs.existsSync(path)
 }
 
-let acount = runSingleExec('git config user.name')
+function removeDir(dir) {
+  let files = fs.readdirSync(dir)
+  for (let i = 0; i < files.length; i++) {
+    let newPath = path.join(dir, files[i]);
+    let stat = fs.statSync(newPath)
+    if (stat.isDirectory()) {
+      //如果是文件夹就递归下去
+      removeDir(newPath);
+    } else {
+      //删除文件
+      fs.unlinkSync(newPath);
+    }
+  }
+  fs.rmdirSync(dir)
+}
 
-console.log(acount)
+function copy(src, dest) {
+  let path = `${cwd}`
+  if(!fileExist(path)) {
+    fs.mkdir(defaultTargetDir, function (error) {
+      if(error) {
+        throw new Error(red('✖') + 'mkdir error, process exit')
+      }
+
+    })
+  }
+}
+
+async function init() {
+
+  let path = `${cwd}/${defaultTargetDir}`
+  let pack = `${cwd}/package.json`
+  let dist = `${cwd}/dist`
+
+  if(!fileExist(pack)) {
+    throw new Error(red('✖') + '位置目录异常, 推送终止')
+  }
+  if(!fileExist(dist)) {
+    throw new Error(red('✖') + '请先构建出dist生产文件后在执行发布')
+  }
+  if (fileExist(path)) {
+    removeDir(path)
+  }
+  copy()
+}
+
+init().catch((e) => {
+  console.error(e)
+})
+
+
