3aef113442581292b2a516ece604af61779f1de5
longyadong <202780181@qq.com>
2023-01-10 10:34:09 +0800

    fix: update.2x

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 import fs from 'node:fs'
 import path from 'node:path'
-import { red, } from 'kolorist'
+import { red, green } from 'kolorist'
 
 const cwd = process.cwd()
 
@@ -11,6 +11,17 @@ const defaultTargetDir = 'publish'
 function fileExist(path) {
   return fs.existsSync(path)
 }
+
+const packagePlate = `
+{
+  "name": "unit",
+  "version": "1.0.0",
+  "files": [
+    "dist"
+  ],
+  "devDependencies": {}
+}
+`
 
 function removeDir(dir) {
   let files = fs.readdirSync(dir)
@@ -33,29 +44,70 @@ const isExist = (path) => {
   }
 }
 
-function copy(src, target) {
-  let path = `${cwd}/dist`
-  let copyPath = `${cwd}/${defaultTargetDir}`
-  if(!fileExist(path)) {
-    fs.mkdir(copyPath, function (error) {
-      if(error) {
-        throw new Error(red('✖') + 'mkdir error, process exit')
+/**
+ * 复制文件夹到目标文件夹
+ * @desc 兼容 node<= 16.7.0
+ * @param {string} src 源目录
+ * @param {string} dest 目标目录
+ * @param {function} callback 回调
+ */
+function copyDirOld(src, dest, callback) {
+  const copy = (copySrc, copyDest) => {
+    fs.readdir(copySrc, (err, list) => {
+      if (err) {
+        callback(err)
+        return;
       }
+      list.forEach(item => {
+        const ss = path.resolve(copySrc, item)
+        fs.stat(ss, (err, stat) => {
+          if (err) {
+            callback(err)
+          } else {
+            const curSrc = path.resolve(copySrc, item)
+            const curDest = path.resolve(copyDest, item)
 
-      const sourceFile = fs.readdirSync(src,  { withFileTypes: true })
-
-      sourceFile.forEach(file=> {
-        const newSourcePath = path.resolve(src, file.name)
-        const newTargetPath = path.resolve(target, file.name)
-
-        if (file.isDirectory()) {
-          isExist(newTargetPath)
-          copy(newSourcePath, newTargetPath)
-        }
-        fs.copyFileSync(newSourcePath, newTargetPath)
+            if (stat.isFile()) {
+              fs.createReadStream(curDest).pipe(fs.createWriteStream(curDest))
+            } else if (stat.isDirectory()) {
+              fs.mkdirSync(curDest, {recursive: true})
+              copy(curSrc, curDest)
+            }
+          }
+        })
       })
     })
-    console.log(path.resolve(__dirname, './src'))
+  }
+  fs.access(dest, (err) => {
+    if (err) {
+      fs.mkdirSync(dest, {recursive: true})
+    }
+    copy(src, dest)
+  })
+}
+
+function copyToDir(srcDir, destDir) {
+  // node >= 16.7.0
+  fs.cp(srcDir, destDir, {recursive: true}, (err) => {
+    if (err) {
+      console.log(red('✖ 文件复制失败,请重试'))
+      process.exit(0)
+    }
+  });
+  console.log(green('✔️文件复制完成'))
+}
+
+function copy(src, target) {
+  let dist = `${cwd}/dist`
+  let copyPath = `${cwd}/${defaultTargetDir}`
+  if (fileExist(dist)) {
+    fs.mkdir(copyPath, function (error) {
+      if (error) {
+        console.log(red('✖ mkdir error, process exit'))
+        process.exit(0)
+      }
+      copyToDir(src, target)
+    })
   }
 }
 
@@ -65,11 +117,13 @@ async function init() {
   let pack = `${cwd}/package.json`
   let dist = `${cwd}/dist`
 
-  if(!fileExist(pack)) {
-    throw new Error(red('✖') + '位置目录异常, 推送终止')
+  if (!fileExist(pack)) {
+    console.log(red('✖ 位置目录异常, pub-nexus 已终止'))
+    process.exit(0)
   }
-  if(!fileExist(dist)) {
-    throw new Error(red('✖') + '请先构建出dist生产文件后在执行发布')
+  if (!fileExist(dist)) {
+    console.log(red('✖ 请先构建出dist生产文件后 再执行发布'))
+    process.exit(0)
   }
   if (fileExist(path)) {
     removeDir(path)
